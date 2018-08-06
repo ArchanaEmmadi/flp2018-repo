9bd1c601dd8e663fc1f953f62f2713b19fa1c6d2
aemmadi <aemmadi@DIN73002751.corp.capgemini.com>
2018-08-06 12:12:08 +0530

    added sales class

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -2,7 +2,9 @@ package org.cap.demo;
 
 public class MainClass {
 	public static void main(String[] args) {
+		SalesClass class1=new SalesClass();
 		System.out.println("hello world");
+		class1.show();
 	greet();
 }
 public static void greet() {

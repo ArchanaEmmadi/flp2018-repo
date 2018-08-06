ee7ad5c917534bef4daa908a1b13e19e722e7b70
aemmadi <aemmadi@DIN73002751.corp.capgemini.com>
2018-08-06 12:22:37 +0530

    Customer class added

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/CustomerClass.java
@@ -0,0 +1,19 @@
+package org.cap.demo;
+
+public class CustomerClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("customer class");
+	}
+	public void showCust() {
+		for(int i=0;i<3;i++)
+		{
+			System.out.println("customer");
+		}
+
+	}
+}
+			
+
+
--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -3,8 +3,10 @@ package org.cap.demo;
 public class MainClass {
 	public static void main(String[] args) {
 		SalesClass class1=new SalesClass();
+		CustomerClass c=new CustomerClass();
 		System.out.println("hello world");
 		class1.show();
+		c.showCust();
 	greet();
 }
 public static void greet() {
